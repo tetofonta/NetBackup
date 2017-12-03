2ff8d98c854f15c56edeaa1f1ff5d1d01d56ff5b
Stefano <f.ste2000@gmail.com>
2017-12-03 15:51:38 +0100

    Why aren't you working travis?

---
+++ Test/lib/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/stefano/Scrivania/Applicazioni/C C++/NetBackup/Test/lib/googletest/googlemock
+# Build directory: /home/stefano/Scrivania/Applicazioni/C C++/NetBackup/Test/lib/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ Test/lib/googletest/googlemock/cmake_install.cmake
@@ -0,0 +1,59 @@
+# Install script for directory: /home/stefano/Scrivania/Applicazioni/C C++/NetBackup/Test/lib/googletest/googlemock
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "/home/stefano/Scrivania/Applicazioni/C C++/NetBackup/Test/lib/googletest/googlemock/libgmock.a")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "/home/stefano/Scrivania/Applicazioni/C C++/NetBackup/Test/lib/googletest/googlemock/libgmock_main.a")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE DIRECTORY FILES "/home/stefano/Scrivania/Applicazioni/C C++/NetBackup/Test/lib/googletest/googlemock/include/gmock")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig" TYPE FILE FILES
+    "/home/stefano/Scrivania/Applicazioni/C C++/NetBackup/Test/lib/googletest/gmock.pc"
+    "/home/stefano/Scrivania/Applicazioni/C C++/NetBackup/Test/lib/googletest/gmock_main.pc"
+    )
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/home/stefano/Scrivania/Applicazioni/C C++/NetBackup/Test/lib/googletest/googlemock/gtest/cmake_install.cmake")
+
+endif()
+
